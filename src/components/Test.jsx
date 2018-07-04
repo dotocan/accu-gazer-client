93d814e1ef7b4da3cb4a64a2b890f65e98a8d3b5
Dominik Otočan <dotocan@croz.net>
2018-07-04 18:29:51 +0200

    Improved performance by removing multiple setState calls and using state copy.

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -2,30 +2,41 @@ import React from "react";
 import Rectangle from "./Rectangle";
 import instance from "../config/axios";
 /* eslint-disable no-undef */
+var counter = 0;
 
 class Test extends React.Component {
   state = {
-
-    measurements: [],
+    testResult: {
+      screenWidth: 0,
+      screenHeight: 0,
+      measurements: [],
+    },
 
     screenWidth: 0,
     screenHeight: 0,
-    rectX: 0,
-    rectY: 0,
-    rectXOffset: 0,
-    rectYOffset: 0,
-    rectWidth: 0,
-    rectHeight: 0,
     gazerX: 0,
     gazerY: 0,
 
-    settings: {},
-    secondsLeft: 10,
+    rectData: {
+      rectX: 0,
+      rectY: 0,
+      rectWidth: 0,
+      rectHeight: 0
+    },
+
+    settings: {
+      numberOfTests: 0,
+      shuffle: false,
+      testDurationInSeconds: 0
+    },
+
+    testsLeft: 0,
+    secondsLeft: 0,
     divisionFactor: 2,
 
     blinked: false,
     firstBlinkLetter: null,
-    firstBlinkTime: null,
+    firstBlinkTime: null
   };
 
   componentWillMount() {
@@ -34,13 +45,25 @@ class Test extends React.Component {
 
   componentDidMount() {
     this.startWebGazer();
-   // window.addEventListener("resize", this.updateWindowDimensions);
     this.getTestConfig();
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.secondsLeft !== nextState.secondsLeft) {
+      return true;
+    }
+
+    return false;
   }
 
   getTestConfig() {
     instance
-      .get("settings", {headers: {'Authorization': 'Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxIiwidW5pcXVlX25hbWUiOiJ0ZXN0QHRlc3QuY29tIiwibmJmIjoxNTMwNjA4NjcxLCJleHAiOjE1MzA2OTUwNzEsImlhdCI6MTUzMDYwODY3MX0._7DE_VQX9a2hm6ctqfdspGY_qskvIv4yFARkd0Uu8wlUQWILpVaPkEOvyHea16qsqnEDMISqFCxGNzXfQlhSjg'}})
+      .get("settings", {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiIxIiwidW5pcXVlX25hbWUiOiJ0ZXN0QHRlc3QuY29tIiwibmJmIjoxNTMwNjA4NjcxLCJleHAiOjE1MzA2OTUwNzEsImlhdCI6MTUzMDYwODY3MX0._7DE_VQX9a2hm6ctqfdspGY_qskvIv4yFARkd0Uu8wlUQWILpVaPkEOvyHea16qsqnEDMISqFCxGNzXfQlhSjg"
+        }
+      })
       .then(res => this.handleResponse(res))
       .catch(err => console.log(err));
   }
@@ -51,81 +74,99 @@ class Test extends React.Component {
   }
 
   handleResponse = response => {
-    console.log(response)
-    this.setState({ settings: response.data })
-    this.setState({ secondsLeft: this.state.settings.testDurationInSeconds })
+    const newState = {...this.state};
 
-    this.calculateRectData();
+    newState.settings = response.data;
+    newState.secondsLeft = response.data.testDurationInSeconds;
+    newState.testsLeft = response.data.numberOfTests;
 
-    this.interval = setInterval(() => this.runEverySecond(), 1000);
+    this.calculateRectData(newState);
+    this.setState({ ...newState });
+
     this.interval = setInterval(() => this.runEveryHalfSecond(), 500);
-  }
+  };
 
   runEveryHalfSecond() {
-    console.log(webgazer.getCurrentPrediction());
-
-    const measurements = this.state.measurements;
+    // Save current measurement data
+    const measurements = this.state.testResult.measurements;
     const measurement = {
-      rectX: this.state.rectX,
-      rectY: this.state.rectY,
-      rectXOffset: this.state.rectXOffset,
-      rectYOffset: this.state.rectYOffset,
-      rectWidth: this.state.rectWidth,
-      rectHeight: this.state.rectHeight,
+      rectData: this.state.rectData,
       gazer: webgazer.getCurrentPrediction(),
-      date: new Date(),
-    }
+      date: new Date().getTime()
+    };
 
     measurements.push(measurement);
-    console.log(measurements);
-  }
 
-  shouldComponentUpdate(nextProps, nextState) {
-    if (this.state.settings.numberOfTests !== nextState.settings.numberOfTests) {
-      this.calculateRectData();
-      return true;
+    counter++;
+    if (counter % 2 === 0) {
+      this.runEverySecond();
     }
-    return false;
   }
 
   runEverySecond() {
+    const newState = { ...this.state };
 
-    this.setState ({
-      secondsLeft: this.state.secondsLeft - 1
-    });
+    newState.secondsLeft = newState.secondsLeft - 1;
 
+    if (newState.secondsLeft === 0) {
+      newState.testsLeft = newState.testsLeft - 1;
 
-    if (this.state.secondsLeft === 0) {
-
-      if(this.state.settings.numberOfTests === 0) {
+      if (newState.testsLeft === 0) {
         clearInterval(this.interval);
-        return
+        webgazer.pause();
+
+        this.printResults();
+
+        return;
       }
 
-      const settings = { ...this.state.settings };
-      settings.numberOfTests -= 1;
+      // Calculate new rect data only when seconds reach 0
+      newState.rectData = this.calculateRectData(newState);
 
-      this.setState({
-        secondsLeft: this.state.settings.testDurationInSeconds,
-        settings,
-        divisionFactor: this.state.divisionFactor + 1
-      });
+      // Reset seconds counter and increment division factor
+      newState.secondsLeft = this.state.settings.testDurationInSeconds;
+      newState.divisionFactor = newState.divisionFactor + 1;
+    }
+
+    this.setState({ ...newState });
+  }
+
+  printResults() {
+    const result = {...this.state.testResult};
+    result.screenWidth = this.state.screenWidth;
+    result.screenHeight = this.state.screenHeight;
+
+    for (let measurement of result.measurements) {
+      if (measurement.gazer !== null) {
+        const {x: gazerX, y: gazerY} = measurement.gazer;
+        const {rectWidth, rectHeight, rectX, rectY} = measurement.rectData;
+
+        const rectStartX = rectX;
+        const rectEndX = rectX + rectWidth;
+        const rectStartY = rectY;
+        const rectEndY = rectY + rectHeight;
+
+        const gazerIsInXBounds = (gazerX >= rectStartX) && (gazerX <= rectEndX);
+        const gazerIsInYBounds = (gazerY >= rectStartY) && (gazerY <= rectEndY);
+        if (gazerIsInXBounds && gazerIsInYBounds) console.log("True");
+        else console.log("False");
+      }
     }
   }
 
-  calculateRectData = () => {
-    this.calculateRectangleWidth();
-    this.calculateRectangleHeight();
-    this.calculateRectangleXOffset();
-    this.calculateRectangleYOffset();
-    this.calculateRectangleX();
-    this.calculateRectangleY();
-  }
+  calculateRectData = newState => {
+    newState.rectData.rectWidth = this.calculateRectangleWidth(newState);
+    newState.rectData.rectHeight = this.calculateRectangleHeight(newState);
+    newState.rectData.rectX = this.calculateRectangleX(newState);
+    newState.rectData.rectY = this.calculateRectangleY(newState);
+
+    return newState.rectData;
+  };
 
   componentWillUnmount() {
     clearInterval(this.interval);
-    //window.removeEventListener("resize", this.updateWindowDimensions);
-    webgazer.showPredictionPoints(false).clearGazeListener().pause();
+    webgazer
+      .pause();
   }
 
   updateWindowDimensions = () => {
@@ -135,54 +176,54 @@ class Test extends React.Component {
     });
   };
 
-  calculateRectangleWidth() {
-    this.setState({rectWidth: this.state.screenWidth / this.state.divisionFactor});
+  calculateRectangleWidth(newState) {
+    return newState.screenWidth / newState.divisionFactor;
+  }
+
+  calculateRectangleHeight(newState) {
+    return newState.screenHeight / newState.divisionFactor;
+  }
+
+  calculateRectangleOffset = (min, max, dimension) => {
+    return Math.floor(Math.random() * max + min) / dimension;
   };
 
-  calculateRectangleHeight() {
-    this.setState({rectHeight: this.state.screenHeight / this.state.divisionFactor});
-  };
+  calculateRectangleX(newState) {
+    const minX = 0;
+    const maxX = newState.screenWidth - newState.rectData.rectWidth;
 
-  calculateRectangleXOffset() {
-    // Avoid showing rectangle out of screen
-    const min = 0;
-    const max = this.state.screenWidth - this.state.rectWidth;
+    const rectXOffset = this.calculateRectangleOffset(
+      minX,
+      maxX,
+      newState.screenWidth
+    );
 
-    // Show rectangle on random x coordinate within defined bounds
-    const randomXOffset =
-      Math.floor(Math.random() * max + min) / this.state.screenWidth;
-    this.setState({ rectXOffset: randomXOffset });
-  };
+    return newState.screenWidth * rectXOffset;
+  }
 
-  calculateRectangleYOffset() {
-     // Avoid showing rectangle out of screen
-    const min = 0;
-    const max = this.state.screenHeight - this.state.rectHeight;
+  calculateRectangleY(newState) {
+    const minY = 0;
+    const maxY = newState.screenHeight - newState.rectData.rectHeight;
 
-    // Show rectangle on random y coordinate within defined bounds
-    const randomYOffset =
-      Math.floor(Math.random() * max + min) / this.state.screenHeight;
-    this.setState({ rectYOffset: randomYOffset });
-  };
+    const rectYOffset = this.calculateRectangleOffset(
+      minY,
+      maxY,
+      newState.screenHeight
+    );
 
-  calculateRectangleX() {
-    this.setState({rectX: this.state.screenWidth * this.state.rectXOffset});
-  };
-
-  calculateRectangleY() {
-    this.setState({rectY: this.state.screenHeight * this.state.rectYOffset});
-  };
+    return newState.screenHeight * rectYOffset;
+  }
 
   render() {
     return (
       <React.Fragment>
-        <p>Number of tests left: {this.state.settings.numberOfTests}</p>
+        <p>Number of tests left: {this.state.testsLeft}</p>
         <p>Seconds left: {this.state.secondsLeft}</p>
         <Rectangle
-          width={this.state.rectWidth}
-          height={this.state.rectHeight}
-          x={this.state.rectX}
-          y={this.state.rectY}
+          width={this.state.rectData.rectWidth}
+          height={this.state.rectData.rectHeight}
+          x={this.state.rectData.rectX}
+          y={this.state.rectData.rectY}
         />
       </React.Fragment>
     );
