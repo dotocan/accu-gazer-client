cdb583dda999ad28067cdb0e0a5c33f1f77ed9ae
DOTOCAN\dotocan <dotocan@croz.net>
2018-09-06 10:50:36 +0200

    Fixed date picker

--- src/components/Auth/RegistrationForm.jsx
+++ src/components/Auth/RegistrationForm.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import instance from "../../config/axios";
+import DatePicker from "react-datepicker";
+import moment from "moment";
 
 class RegistrationForm extends React.Component {
   state = {
     firstName: "",
     lastName: "",
     gender: "male",
-    dateOfBirth: 0,
+    startDate: moment(),
+    dateOfBirth: moment(),
     email: "",
     password: ""
   };
@@ -18,7 +21,7 @@ class RegistrationForm extends React.Component {
       firstName: this.state.firstName,
       lastName: this.state.lastName,
       gender: this.state.gender,
-      dateOfBirth: this.state.dateOfBirth,
+      dateOfBirth: this.state.dateOfBirth.unix(),
       email: this.state.email,
       password: this.state.password
     });
@@ -50,9 +53,8 @@ class RegistrationForm extends React.Component {
     this.setState({ gender });
   };
 
-  handleDateOfBirthChange = event => {
-    const dateOfBirth = event.target.value;
-    this.setState({ dateOfBirth });
+  handleDateOfBirthChange = date => {
+    this.setState({ dateOfBirth: date });
   };
 
   handleEmailChange = event => {
@@ -68,39 +70,85 @@ class RegistrationForm extends React.Component {
   render() {
     return (
       <div className="text-center container h-100 w-100 center-vertical">
-            <form className="col col-sm-10 col-lg-6" onSubmit={this.handleSubmit}>
-                <h1 className="h3 mb-4 font-weight-normal">Create account</h1>
+        <form className="col col-sm-10 col-lg-6" onSubmit={this.handleSubmit}>
+          <h1 className="h3 mb-4 font-weight-normal">Create account</h1>
 
-                <input className="form-control my-2" type="text" placeholder="First Name" value={this.state.firstName} onChange={this.handleFirstNameChange}
-                />
-                <input className="form-control my-2" type="text" placeholder="Last Name" value={this.state.lastName} onChange={this.handleLastNameChange}
-                />
+          <input
+            className="form-control my-2"
+            type="text"
+            placeholder="First Name"
+            value={this.state.firstName}
+            onChange={this.handleFirstNameChange}
+          />
 
-                <div className="custom-control custom-control-inline custom-radio">
-                    <input className="custom-control-input" type="radio" name="inlineRadioOptions" id="maleRadio" value="male" checked={this.state.gender==="male"
-                        } onChange={this.handleGenderChange} />
-                    <label className="custom-control-label" htmlFor="maleRadio">
-                        Male
-                    </label>
-                </div>
-                <div className="custom-control custom-control-inline custom-radio">
-                    <input className="custom-control-input" type="radio" name="inlineRadioOptions" id="femaleRadio" value="female" checked={this.state.gender==="female"
-                        } onChange={this.handleGenderChange} />
-                    <label className="custom-control-label" htmlFor="femaleRadio">
-                        Female
-                    </label>
-                </div>
+          <input
+            className="form-control my-2"
+            type="text"
+            placeholder="Last Name"
+            value={this.state.lastName}
+            onChange={this.handleLastNameChange}
+          />
 
-                <input className="form-control my-2" type="date" placeholder="Date of Birth" value={this.state.dateOfBirth} onChange={this.handleDateOfBirthChange}
-                />
-                <input className="form-control my-2" type="text" placeholder="E-mail" value={this.state.email} onChange={this.handleEmailChange}
-                />
-                <input className="form-control my-2" type="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange}
-                />
-                <button className="btn btn-lg btn-primary btn-block my-4" type="submit">
-                    Sign Up
-                </button>
-            </form>
+          <DatePicker
+            className="form-control my-2"
+            dateFormat="DD/MM/YYYY"
+            selected={this.state.startDate}
+            onChangeRaw={this.handleDateOfBirthChange}
+          />
+
+          <div className="custom-control custom-control-inline custom-radio">
+            <input
+              className="custom-control-input"
+              type="radio"
+              name="inlineRadioOptions"
+              id="maleRadio"
+              value="male"
+              checked={this.state.gender === "male"}
+              onChange={this.handleGenderChange}
+            />
+            <label className="custom-control-label" htmlFor="maleRadio">
+              Male
+            </label>
+          </div>
+
+          <div className="custom-control custom-control-inline custom-radio">
+            <input
+              className="custom-control-input"
+              type="radio"
+              name="inlineRadioOptions"
+              id="femaleRadio"
+              value="female"
+              checked={this.state.gender === "female"}
+              onChange={this.handleGenderChange}
+            />
+            <label className="custom-control-label" htmlFor="femaleRadio">
+              Female
+            </label>
+          </div>
+
+          <input
+            className="form-control my-2"
+            type="text"
+            placeholder="E-mail"
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+          />
+
+          <input
+            className="form-control my-2"
+            type="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
+
+          <button
+            className="btn btn-lg btn-primary btn-block my-4"
+            type="submit"
+          >
+            Sign Up
+          </button>
+        </form>
       </div>
     );
   }
